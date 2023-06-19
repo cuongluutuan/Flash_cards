dce9c681e6329cac81b555bc1a65c4f4d9947e6b
kaitozu <43519768+kaitoz11@users.noreply.github.com>
2023-06-19 22:09:25 +0700

    feat: more stuff

---
+++ Services/Repository/BaseReposity.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Repository
+{
+    public class BaseReposity<TEntity> where TEntity : class
+    {
+        public MyApplicationDBContext _context;
+        public DbSet<TEntity> _dbSet;
+
+        public BaseReposity(MyApplicationDBContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public List<TEntity> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public void Create(TEntity item)
+        {
+            _dbSet.Add(item);
+        }
+
+        public bool Delete(TEntity item)
+        {
+            try
+            {
+                _dbSet.Remove(item);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void Update(TEntity item)
+        {
+            var tracker = _context.Attach(item);
+            tracker.State = EntityState.Modified;
+        }
+
+    }
+}
